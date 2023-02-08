abd811292fc330f889d0a06756c8c44d6ddb481a
Dema2096 <mrrh96@gmail.com>
2023-02-08 06:01:09 -0300

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors({
     origin:["http://localhost:3000", "https://mern-task-app-fzer.onrender.com"]
 }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use("/api/tasks",tasksRoutes)
 
 
